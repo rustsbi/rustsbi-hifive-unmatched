d0bf02da2a2d506ad7d97a0a2ab6f7e2c85369f3
luojia65 <me@luojia.cc>
2021-11-09 13:44:40 +0800

    编译到ELF文件

---
+++ build.rs
@@ -0,0 +1,17 @@
+use std::env;
+use std::fs;
+use std::io::Write;
+use std::path::PathBuf;
+
+fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=src/u740.ld");
+
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    fs::File::create(out_dir.join("u740.ld"))
+        .unwrap()
+        .write_all(include_bytes!("src/u740.ld"))
+        .unwrap();
+    println!("cargo:rustc-link-search={}", out_dir.display());
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,51 @@
 #![no_std]
 #![no_main]
+#![feature(naked_functions)]
+#![feature(asm, asm_const, asm_sym)]
+#![feature(generator_trait)]
+#![feature(default_alloc_error_handler)]
+#![no_std]
+#![no_main]
+
+mod runtime;
 
 use core::panic::PanicInfo;
 
 #[panic_handler]
 fn on_panic(_pi: &PanicInfo) -> ! {
     loop {}
+}
+
+const PER_HART_STACK_SIZE: usize = 4 * 4096; // 16KiB
+const SBI_STACK_SIZE: usize = 5 * PER_HART_STACK_SIZE; // 5 harts
+#[link_section = ".bss.uninit"]
+static mut SBI_STACK: [u8; SBI_STACK_SIZE] = [0; SBI_STACK_SIZE];
+
+fn rust_main() -> ! {
+    runtime::init();
+    todo!()
+}
+
+#[naked]
+#[link_section = ".text.entry"]
+#[export_name = "_start"]
+unsafe extern "C" fn entry() -> ! {
+    asm!(
+    // 1. set sp
+    // sp = bootstack + (hartid + 1) * HART_STACK_SIZE
+    "
+    la      sp, {stack}
+    li      t0, {per_hart_stack_size}
+    csrr    t1, mhartid
+    addi    t2, t1, 1
+1:  add     sp, sp, t0
+    addi    t2, t2, -1
+    bnez    t2, 1b
+    ",
+    // 2. jump to rust_main (absolute address)
+    "j      {rust_main}",
+    per_hart_stack_size = const PER_HART_STACK_SIZE,
+    stack = sym SBI_STACK,
+    rust_main = sym rust_main,
+    options(noreturn))
 }
