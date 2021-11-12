1f1fed162136f6a118c640e58b5d17afe93fca5a
luojia65 <me@luojia.cc>
2021-11-12 15:47:14 +0800

    精简链接器脚本设置

--- build.rs
+++ build.rs
@@ -5,14 +5,5 @@ use std::path::PathBuf;
 
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=src/u740.ld");
-
-    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-
-    fs::File::create(out_dir.join("u740.ld"))
-        .unwrap()
-        .write_all(include_bytes!("src/u740.ld"))
-        .unwrap();
-    println!("cargo:rustc-link-arg=-Tu740.ld");
-    println!("cargo:rustc-link-search={}", out_dir.display());
+    println!("cargo:rustc-link-arg=-Tsrc/u740.ld");
 }
