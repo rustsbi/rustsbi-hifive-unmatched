7f0e448298d3d9ef95929194a806a3e1a721502f
luojia65 <me@luojia.cc>
2021-11-30 21:38:12 +0800

    Clean all registers on begin

--- rustsbi-hifive-unmatched/src/main.rs
+++ rustsbi-hifive-unmatched/src/main.rs
@@ -109,7 +109,39 @@ static mut SBI_STACK: [u8; SBI_STACK_SIZE] = [0; SBI_STACK_SIZE];
 #[export_name = "_start"]
 unsafe extern "C" fn entry() -> ! {
     asm!(
-    // 1. set sp
+    // 1. clear all registers
+    "li x1, 0
+    li x2, 0
+    li x3, 0
+    li x4, 0
+    li x5, 0
+    li x6, 0
+    li x7, 0
+    li x8, 0
+    li x9, 0
+    li x10, 0
+    li x11, 0
+    li x12, 0
+    li x13, 0
+    li x14, 0
+    li x15, 0
+    li x16, 0
+    li x17, 0
+    li x18, 0
+    li x19, 0
+    li x20, 0
+    li x21, 0
+    li x22, 0
+    li x23, 0
+    li x24, 0
+    li x25, 0
+    li x26, 0
+    li x27, 0
+    li x28, 0
+    li x29, 0
+    li x30, 0
+    li x31, 0",
+    // 2. set sp
     // sp = bootstack + (hartid + 1) * HART_STACK_SIZE
     "
     la      sp, {stack}
@@ -120,7 +152,7 @@ unsafe extern "C" fn entry() -> ! {
     addi    t2, t2, -1
     bnez    t2, 1b
     ",
-    // 2. jump to rust_main (absolute address)
+    // 3. jump to main function (absolute address)
     "j      {rust_main}",
     per_hart_stack_size = const PER_HART_STACK_SIZE,
     stack = sym SBI_STACK,
