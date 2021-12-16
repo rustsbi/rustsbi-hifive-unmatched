ab0bc3423fdb344fabe3dbcd8b30b8d2af5735fb
luojia65 <me@luojia.cc>
2021-12-16 22:34:13 +0800

    小bug修复

--- rustsbi-hifive-unmatched/src/device_tree.rs
+++ rustsbi-hifive-unmatched/src/device_tree.rs
@@ -4,6 +4,7 @@ use alloc::collections::BTreeMap;
 
 #[derive(Debug, Deserialize)]
 struct Tree<'a> {
+    #[serde(borrow)]
     aliases: BTreeMap<&'a str, &'a str>,
     chosen: Option<Chosen<'a>>,
 }
@@ -14,10 +15,8 @@ struct Chosen<'a> {
     stdout_path: Option<&'a str>,
 }
 
-use crate::peripheral::Uart;
-
 pub unsafe fn parse_device_tree(dtb_pa: usize) -> Result<()> {
-    let tree = serde_device_tree::from_raw(dtb_pa as *const u8)?;
+    let tree: Tree = serde_device_tree::from_raw(dtb_pa as *const u8)?;
     use crate::console::println;
     if let Some(chosen) = tree.chosen {
         if let Some(stdout_path) = chosen.stdout_path {
